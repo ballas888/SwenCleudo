4fad418321aeec531d2d61ef09a42f76e431f282
Ballas888 <ritchierunner@gmail.com>
2014-08-10 18:08:22 -0700

    Stuff

---
+++ Cluedo/src/cluedo/character/CharsName.java
@@ -0,0 +1,6 @@
+package cluedo.character;
+
+public enum CharsName{
+	MRS_WHITE, MISS_SCARLET, COLONEL_MUSTARD, REVEREND_GREEN,
+	MRS_PEACOCK, PROFESSOR_PLUM
+}
--- Cluedo/src/cluedo/main/Game.java
+++ Cluedo/src/cluedo/main/Game.java
@@ -159,6 +159,7 @@ public class Game implements Runnable {
 		KITCHEN, BALL_ROOM, CONSERVATORY, DINING_ROOM, BILLARD_ROOM,
 		LIBRARY, LOUNGE, HALL, STUDY, FLOOR
 	}
+	
 	//private int x = 0, y = 0;
 
 	public static void main(String[] args) {
